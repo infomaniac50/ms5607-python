6b442db069b46dd764aaeb00edd8281b2195708a
Derek Chafin <infomaniac50@gmail.com>
2020-09-18 19:33:37 -0500

    We need to compensate for the time it takes to read the sensor.
    If the climb rate is 20cm in 0.5 seconds, then over 1 second the climb rate is 40cm. If the climb rate 20cm in 2 seconds, then over 1 second the climb rate is 10cm. I'm not entirely sure the code is right but it should match the method above. The climb rate calculations need more testing that I'm not doing right now.

--- altitude.py
+++ altitude.py
@@ -1,12 +1,16 @@
 import ms5607
+import time
 
 s = ms5607.MS5607()
 old_altitude = 0.0
 
 while True:
+    sense_start = time.time()
     # Reading the sensor and calculating the altitude takes a significant amount of time,
-    # so no call to time.sleep is included here.
+    # so we will also measure how long it takes to calculate.
     altitude = s.read_altitude(samples=10, pressure_osr=4096, temperature_osr=256)
+    sense_end = time.time()
+
     # Limit altitude to 20cm increments.
     altitude = int(altitude * 5) / 5
 
@@ -14,8 +18,13 @@ while True:
 
     # Calculate climb rate by taking the difference from the current altitude by the old altitude.
     # Convert altitude to centimeters
-    climb_rate = round((altitude - old_altitude) * 100)
+    climb_rate = (altitude - old_altitude) * 100
 
-    print("climb rate={} cm/sec".format(climb_rate))
+    # Because the sensor takes so long we need to compensate for this.
+    # Our unit is centimeters per second.
+    # We need to subtract the time it took to read our measurement from our unit.
+    sec_unit = sense_end - sense_start
+    true_climb_rate = round(climb_rate * sec_unit)
+    print("climb rate={} cm/sec".format(true_climb_rate))
 
     old_altitude = altitude
